b185ab7511cee27608bb9ca206e089a46d44c186
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-05-16 13:44:38 -0500

    agregue la funcion del menu y lo estilice

---
+++ main.js
@@ -0,0 +1,7 @@
+const iconHamburger = document.querySelector('.icono_hamburger');
+const navOverlay = document.querySelector('.nav_overlay');
+
+iconHamburger.addEventListener('click', () => { 
+    iconHamburger.classList.toggle('icono_hamburger--open');
+    navOverlay.classList.toggle('nav_overlay--show');
+})
