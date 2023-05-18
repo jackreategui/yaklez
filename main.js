5514ef8e7fedba35756840206fe833af522c94e1
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-05-18 11:16:00 -0500

    agregue las animaciones de css al overlay del menu

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const navOverlay = document.querySelector('.nav_overlay');
 iconHamburger.addEventListener('click', () => { 
     iconHamburger.classList.toggle('icono_hamburger--open');
     navOverlay.classList.toggle('nav_overlay--show');
+    navOverlay.classList.toggle('animate__slideInRight');
 })
