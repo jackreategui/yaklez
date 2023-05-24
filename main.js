63b5736891ad8134707d0767a2f5dbcad84d04e1
Jack Reategui PC <jackreategui@gmail.com>
2023-05-23 23:41:08 -0500

    mejore el css de las paginas y el nav, ademas termine la pagina de servicios

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ AOS.init();
 
 const iconHamburger = document.querySelector('.icono_hamburger');
 const navOverlay = document.querySelector('.nav_overlay');
+const navContainer = document.querySelector('.nav_container');
 
 iconHamburger.addEventListener('click', () => { 
     iconHamburger.classList.toggle('icono_hamburger--open');
     navOverlay.classList.toggle('nav_overlay--show');
     navOverlay.classList.toggle('animate__slideInRight');
+})
+
+navOverlay.addEventListener('click', () => {
+    iconHamburger.classList.remove('icono_hamburger--open');
+    navOverlay.classList.remove('nav_overlay--show');
+    navOverlay.classList.remove('animate__slideInRight');
 })
