ab87660cf9c304760f8e93274429336d6d9688e5
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-06-06 19:34:03 -0500

    agregue archivos html aparte ademas que hice dinamico el header y el footer

--- main.js
+++ main.js
@@ -7,57 +7,20 @@ const navContainer = document.querySelector('.nav_container');
 const headerContainer = document.getElementById('header');
 const footerContainer = document.getElementById('footer');
 
-var headerHTML = `<nav>
-// <div class="nav_container">
-//     <a href="./index.html">
-//         <img src="./assets/icon/logo_white.svg" alt="logo de la agencia digital yaklez" class="logo_nav">
-//     </a>
-    
-//     <div class="icono_hamburger" id="iconHamburger"></div>
-    
-//     <div class="nav_overlay animate__animated">
-//         <ul class="nav_ul">
-//             <li class="nav_li">
-//                 <a href="./servicios.html">Servicios</a>
-//             </li>
-//             <li class="nav_li">
-//                 <a href="./proyectos.html">Trabajos</a>
-//             </li>
-//             <li class="nav_li">
-//                 <a target="_blank" href="https://forms.gle/XbYQjxGC3eV562kz5">Contacto</a>
-//             </li>
-//         </ul>
-//     </div>
-// </div>
-// </nav>`;
-var footerHTML = `<div class="footer_container">
-<a href="./index.html">
-    <img src="./assets/icon/logo_blacksvg.svg" alt="logo de la agencia digital yaklez" class="logo_footer">
-</a>
+// Carga el encabezado
+fetch('./contenedores_html/header.html')
+    .then(response => response.text())
+    .then(data => {
+        headerContainer.innerHTML = data;
+    });
 
-<div class="footer_info">
-    <div class="info sobreNosotros">
-        <h4>Sobre Nosotros</h4>
-        <a href="./fag.html">Preguntas frecuentes</a>
-        <a href="./herramientas.html">Herramientas de trabajo</a>
-    </div>
-    <div class="info servicios_footer">
-        <h4>Servicios</h4>
-        <a href="./servicios.html">Diseño digital</a>
-        <a href="./servicios.html">Desarrollo web</a>
-        <a href="./servicios.html">Marketing digital</a>
-        <a href="./servicios.html">Asesoramiento</a>
-    </div>
-    <div class="info recursos">
-        <h4>Recursos</h4>
-        <a href="#">Gratuitos</a>
-        <a href="#">Pagados</a>
-    </div>
-</div>
-</div>`;
+// Carga el pie de página
+fetch('./contenedores_html/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        footerContainer.innerHTML = data;
+    });
 
-headerContainer.innerHTML = headerHTML;
-footerContainer.innerHTML = footerHTML;
 
 iconHamburger.addEventListener('click', () => { 
     iconHamburger.classList.toggle('icono_hamburger--open');
