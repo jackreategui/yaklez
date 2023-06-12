9fd7cc14c06d2ae6a1c8061a65c29bd7e1475ce3
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-06-12 11:42:57 -0500

    agregue un contacto adaptativo

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ AOS.init();
 
 const headerContainer = document.getElementById('header');
 const footerContainer = document.getElementById('footer');
+const contactoContainer = document.getElementById('contacto');
 
 // Carga el encabezado
 fetch('./contenedores_html/header.html')
@@ -15,6 +16,13 @@ fetch('./contenedores_html/footer.html')
     .then(response => response.text())
     .then(data => {
         footerContainer.innerHTML = data;
+    });
+
+// Carga contacto
+fetch('./contenedores_html/contacto.html')
+    .then(response => response.text())
+    .then(data => {
+        contactoContainer.innerHTML = data;
     });
 
     
