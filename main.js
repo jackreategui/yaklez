ff16fdf19060ba27e8ba71a991f01ac1a85b2146
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-06-13 13:06:09 -0500

    arreglo de menu

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 AOS.init();
 
-window.addEventListener('load', function () {
+// window.addEventListener('load', function () {
+//     const iconHamburger = document.querySelector('.icono_hamburger');
+//     const navOverlay = document.querySelector('.nav_overlay');
+        
+//     iconHamburger.addEventListener('click', () => { 
+//         iconHamburger.classList.toggle('icono_hamburger--open');
+//         navOverlay.classList.toggle('nav_overlay--show');
+//         navOverlay.classList.toggle('animate__slideInRight');
+//     })
+    
+//     navOverlay.addEventListener('click', () => {
+//         iconHamburger.classList.remove('icono_hamburger--open');
+//         navOverlay.classList.remove('nav_overlay--show');
+//         navOverlay.classList.remove('animate__slideInRight');
+//     })
+// });
+
+window.onload = function () {
     const iconHamburger = document.querySelector('.icono_hamburger');
     const navOverlay = document.querySelector('.nav_overlay');
         
@@ -15,5 +32,4 @@ window.addEventListener('load', function () {
         navOverlay.classList.remove('nav_overlay--show');
         navOverlay.classList.remove('animate__slideInRight');
     })
-    
-});
+};
