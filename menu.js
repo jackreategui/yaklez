d111012eb548cc9aa3fc62895298658821419bf5
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-06-13 12:43:31 -0500

    agregue un nuevo metodo

--- menu.js
+++ menu.js
@@ -1,4 +1,22 @@
-window.addEventListener('load', function () {
+// window.addEventListener('load', function () {
+//     const iconHamburger = document.querySelector('.icono_hamburger');
+//     const navOverlay = document.querySelector('.nav_overlay');
+        
+//     iconHamburger.addEventListener('click', () => { 
+//         iconHamburger.classList.toggle('icono_hamburger--open');
+//         navOverlay.classList.toggle('nav_overlay--show');
+//         navOverlay.classList.toggle('animate__slideInRight');
+//     })
+    
+//     navOverlay.addEventListener('click', () => {
+//         iconHamburger.classList.remove('icono_hamburger--open');
+//         navOverlay.classList.remove('nav_overlay--show');
+//         navOverlay.classList.remove('animate__slideInRight');
+//     })
+    
+// });
+
+window.onload = function () {
     const iconHamburger = document.querySelector('.icono_hamburger');
     const navOverlay = document.querySelector('.nav_overlay');
         
@@ -14,4 +32,4 @@ window.addEventListener('load', function () {
         navOverlay.classList.remove('animate__slideInRight');
     })
     
-});
+};
